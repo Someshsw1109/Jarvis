6a3d5db855d6eebbf2aeda31d6cfd733f4778efe
Somesh Raj <123828588+Someshsw1109@users.noreply.github.com>
2023-01-29 02:23:05 +0530

    Add files via upload

---
+++ speak+takecommand.py
@@ -0,0 +1,37 @@
+import pyttsx3   
+import speech_recognition as sr
+
+
+
+
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+print(voices[0].id)
+engine.setProperty('voice', voices[0].id)
+
+
+def speak(audio):
+    engine.say(audio)
+    print(audio)
+    engine.runAndWait()
+
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source,timeout=5,phrase_time_limit=5)
+
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, language='en-in')
+        print(f"user said: {query}")
+
+    except Exception as e:
+        speak("sir if you don't mind can you say that again please....")
+        return "None"
+    
+    query = query.lower()
+
+    return query
